5f8cfb3f404673dbbaad3fa7fbfbb2d841432606
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-05-16 18:41:18 -0600

    working with lists

--- random.py
+++ random.py
@@ -8,3 +8,13 @@ if(HorT == 1):
     print("Tails!")
 else:
     print("Heads!")
+
+  
+########################################################3
+import random #random module
+
+##random fruit generator
+fruits = ["apple","grape","watermelon", "melon", "mango"]
+length = len(fruits)
+fruitChoice = random.randint(1,length-1)
+print(f"Your fruit is: {fruits[fruitChoice]}")

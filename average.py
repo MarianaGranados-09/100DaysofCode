1585c207b69f24b95acb241ee77db836c6b897a2
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-05-20 00:55:06 -0600

    Update average.py
    add lowest score program

--- average.py
+++ average.py
@@ -9,3 +9,16 @@ for i in range(0, len(student_ages)):
 #print(student_ages)
 avr = total / len(student_ages)
 print(f"The average age is: {avr}")
+
+#calculating the lowest score
+
+scores = input("Enter a list of scores: ").split()
+lowest = int(scores[0])
+for i in range(0, len(scores)):
+    if lowest > int(scores[i]):
+        lowest = int(scores[i])
+    else:
+        lowest = lowest
+
+print(f"The lowest score is: {lowest}")
+
